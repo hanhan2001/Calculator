2a30b3ed65c471a9a3eba8b0cfc7cb63b25c7eb8
hanhan <764932129@qq.com>
2024-10-11 20:12:11 +0800

    feat: update test code

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,12 +1,22 @@
 import me.xiaoying.calculator.Calculator;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 //        String content = "2 * (5 + 7) + {6 * (9 - 6)}";
 //        long ms = System.currentTimeMillis();
-        String content = "6 + 9 - 50 - 8 + 90 + 999 + 50 + 989 + 1513 + 4561 + 891123";
+//        String content = "6 + 9 - 50 - 8 + 90 + 999 + 50 + 989 + 1513 + 4561 + 891123";
+
+        Scanner scanner = new Scanner(System.in);
         Calculator calculator = new Calculator();
-        calculator.calculator(content);
-//        System.out.println(System.currentTimeMillis() - ms + "ms");
+        String content;
+        while (true) {
+            content = scanner.nextLine();
+            if (content.equalsIgnoreCase("exit"))
+                break;
+
+            System.out.println(calculator.calculator(content));
+        }
     }
 }
